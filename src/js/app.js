e55ea680971c5b7e9cde52814f3da72060726aac
Mihir Chaturvedi <plibither8@gmail.com>
2018-03-14 22:33:13 +0530

    Change page title with change of state

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 var routerApp = angular.module('routerApp', ['ui.router', 'angular-loading-bar'])
 	.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
 		cfpLoadingBarProvider.includeSpinner = false;
+	}])
+
+	.run(['$rootScope', '$state', '$stateParams',
+		function ($rootScope, $state, $stateParams) {
+			$rootScope.$state = $state;
+			$rootScope.$stateParams = $stateParams;
 	}]);
 
 routerApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -11,32 +17,38 @@ routerApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider
 
 		.state('home', {
 			url: '/home',
-			templateUrl: 'partials/home.html'
+			templateUrl: 'partials/home.html',
+			data: { pageTitle: 'ProjectBeta 2018' }
 		})
 
 		.state('about', {
 			url: '/about',
-			templateUrl: 'partials/about.html'
+			templateUrl: 'partials/about.html',
+			data: { pageTitle: 'About \u00B7 ProjectBeta' }
 		})
 
 		.state('contact', {
 			url: '/contact',
-			templateUrl: 'partials/contact.html'
+			templateUrl: 'partials/contact.html',
+			data: { pageTitle: 'Contact \u00B7 ProjectBeta' }
 		})
 
 		.state('team', {
-			url: '/team',
-			templateUrl: 'partials/team.html'
+			url: '/about/team',
+			templateUrl: 'partials/team.html',
+    		data: { pageTitle: 'Team \u00B7 ProjectBeta' }
 		})
 
 		.state('history', {
-			url: '/history',
-			templateUrl: 'partials/history.html'
+			url: '/about/history',
+			templateUrl: 'partials/history.html',
+    		data: { pageTitle: 'History \u00B7 ProjectBeta' }
 		})
 
 		.state('alumni', {
-			url: '/alumni',
-			templateUrl: 'partials/alumni.html'
+			url: '/about/alumni',
+			templateUrl: 'partials/alumni.html',
+    		data: { pageTitle: 'Alumni \u00B7 ProjectBeta' }
 		});
 	
 	// $locationProvider.html5Mode(true);
