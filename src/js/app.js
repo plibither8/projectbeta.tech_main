2d48b5c31189f7bdafefe9d5ca51674097b38d6f
Mihir Chaturvedi <plibither8@gmail.com>
2018-03-15 11:09:25 +0530

    Finalise .htaccess file for clean URL. Add 404  page.

--- src/js/app.js
+++ src/js/app.js
@@ -11,45 +11,87 @@ var routerApp = angular.module('routerApp', ['ui.router', 'angular-loading-bar']
 
 routerApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-	$urlRouterProvider.otherwise('/home');
+	$urlRouterProvider.otherwise('/404');
 
 	$stateProvider
+
+		.state('index', {
+			url: '/',
+			templateUrl: 'partials/home.html',
+			data: { pageTitle: 'ProjectBeta 2018' }
+		})
+
+		.state('404', {
+			url: '/404',
+			templateUrl: 'partials/404.html',
+			data: { pageTitle: '404 \u00B7 ProjectBeta 2018' }
+		})
 
 		.state('home', {
 			url: '/home',
 			templateUrl: 'partials/home.html',
-			data: { pageTitle: 'ProjectBeta 2018' }
+			data: { pageTitle: 'ProjectBeta 2018' },
+			onEnter: function() {
+				if (isMobile) {
+					setTimeout(sidebarClose, 100);
+				}
+			}
 		})
 
 		.state('about', {
 			url: '/about',
 			templateUrl: 'partials/about.html',
-			data: { pageTitle: 'About \u00B7 ProjectBeta' }
+			data: { pageTitle: 'About \u00B7 ProjectBeta' },
+			onEnter: function() {
+				if (isMobile) {
+					setTimeout(sidebarClose, 100);
+				}
+			}
 		})
 
 		.state('contact', {
 			url: '/contact',
 			templateUrl: 'partials/contact.html',
-			data: { pageTitle: 'Contact \u00B7 ProjectBeta' }
+			data: { pageTitle: 'Contact \u00B7 ProjectBeta' },
+			onEnter: function() {
+				if (isMobile) {
+					setTimeout(sidebarClose, 100);
+				}
+			}
 		})
 
 		.state('team', {
 			url: '/about/team',
 			templateUrl: 'partials/team.html',
-    		data: { pageTitle: 'Team \u00B7 ProjectBeta' }
+			data: { pageTitle: 'Team \u00B7 ProjectBeta' },
+			onEnter: function() {
+				if (isMobile) {
+					setTimeout(sidebarClose, 100);
+				}
+			}
 		})
 
 		.state('history', {
 			url: '/about/history',
 			templateUrl: 'partials/history.html',
-    		data: { pageTitle: 'History \u00B7 ProjectBeta' }
+			data: { pageTitle: 'History \u00B7 ProjectBeta' },
+			onEnter: function() {
+				if (isMobile) {
+					setTimeout(sidebarClose, 100);
+				}
+			}
 		})
 
 		.state('alumni', {
 			url: '/about/alumni',
 			templateUrl: 'partials/alumni.html',
-    		data: { pageTitle: 'Alumni \u00B7 ProjectBeta' }
+			data: { pageTitle: 'Alumni \u00B7 ProjectBeta' },
+			onEnter: function() {
+				if (isMobile) {
+					setTimeout(sidebarClose, 100);
+				}
+			}
 		});
 	
-	// $locationProvider.html5Mode(true);
+	$locationProvider.html5Mode(true).hashPrefix('!');
 });
