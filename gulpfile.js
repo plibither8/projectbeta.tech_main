ea94c310f7d565a5a6cd38b09a0def1db7129f74
Mihir Chaturvedi <plibither8@gmail.com>
2018-03-17 12:53:11 +0530

    Enable souremaps on styl files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require("gulp-babel");
 const concat = require("gulp-concat");
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const sourcemaps = require('gulp-sourcemaps');
 const stylus = require("gulp-stylus");
 const surge = require("gulp-surge");
 const webserver = require("gulp-webserver");
@@ -17,18 +18,20 @@ gulp.task('webserver', function () {
 
 gulp.task('pug', function() {
 	return gulp.src('src/pug/pages/**/*.pug')
-	.pipe(pug())
+	.pipe(pug())	
 	.pipe(gulp.dest('dist/'));
 })
 
 gulp.task('stylus', function() {
 	return gulp.src('src/styl/*.styl')
+	.pipe(sourcemaps.init())
 	.pipe(stylus({
 		compress: true
 	}))
 	.pipe(autoprefix({
 		cascade: false
 	}))
+	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('dist/assets/css'));
 })
 
@@ -48,10 +51,6 @@ gulp.task('deploy', function() {
 		project: './dist/',
 		domain: 'projectbeta.surge.sh'
 	})
-})
-
-gulp.task('deployWatch', ['pug', 'stylus', 'scripts', 'deploy'], function() {
-	gulp.watch(['src/pug/**/*.pug', 'src/styl/**/*.styl', 'src/js/**/*.js'], ['pug', 'stylus', 'scripts', 'deploy']);
 })
 
 gulp.task('default', ['webserver', 'pug', 'stylus', 'scripts'], function () {
