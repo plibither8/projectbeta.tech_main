89fbabdcf582b6d93262c1a0a2b19ed4ef800302
Mihir Chaturvedi <plibither8@gmail.com>
2018-03-24 12:36:35 +0530

    Apply small changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 /*jshint esversion: 6 */
 
 const autoprefixer = require("autoprefixer");
-const babel = require("gulp-babel");
-const cachebust = require('gulp-cache-bust');
-const concat = require("gulp-concat");
-const cssnano = require("cssnano");
-const gulp = require("gulp");
-const pug = require("gulp-pug");
-const postcss = require('gulp-postcss');
-const pxtorem = require('postcss-pxtorem');
-const rucksack = require('rucksack-css');
-const sourcemaps = require('gulp-sourcemaps');
-const stylus = require("gulp-stylus");
-const surge = require("gulp-surge");
-const webserver = require("gulp-webserver");
+const babel        = require("gulp-babel");
+const cachebust    = require('gulp-cache-bust');
+const concat       = require("gulp-concat");
+const cssnano      = require("cssnano");
+const gulp         = require("gulp");
+const pug          = require("gulp-pug");
+const postcss      = require('gulp-postcss');
+const pxtorem      = require('postcss-pxtorem');
+const rucksack     = require('rucksack-css');
+const sourcemaps   = require('gulp-sourcemaps');
+const stylus       = require("gulp-stylus");
+const surge        = require("gulp-surge");
+const webserver    = require("gulp-webserver");
 
 gulp.task('webserver', function () {
 	return gulp.src("dist")
 		.pipe(webserver({
 			livereload: true,
 			open: true
-		}))
-})
+		}));
+});
 
 gulp.task('html', function() {
 	return gulp.src('src/pug/pages/**/*.pug')
@@ -30,7 +30,7 @@ gulp.task('html', function() {
 		type: 'timestamp'
 	}))
 	.pipe(gulp.dest('dist/'));
-})
+});
 
 gulp.task('css', function() {
 	return gulp.src('src/styl/*.styl')
@@ -39,7 +39,7 @@ gulp.task('css', function() {
 	.pipe(postcss([autoprefixer, rucksack, pxtorem, cssnano]))
 	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('dist/assets/css'));
-})
+});
 
 gulp.task('js', function() {
 	return gulp.src('src/js/**/*.js')
@@ -50,15 +50,15 @@ gulp.task('js', function() {
 	}))
 	.pipe(concat('scripts.js'))
 	.pipe(gulp.dest('dist/assets/js'));
-})
+});
 
 gulp.task('deploy', function() {
 	return surge({
 		project: './dist/',
 		domain: 'projectbeta.surge.sh'
-	})
-})
+	});
+});
 
 gulp.task('default', ['html', 'css', 'js'], function () {
 	gulp.watch(['src/pug/**/*.pug', 'src/styl/**/*.styl', 'src/js/**/*.js'], ['html', 'css', 'js']);
-})
+});
